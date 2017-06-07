f65c6f8a659fad2d850d55e802737673b6cd7486
Darwin <parisismagic4@hotmail.com>
2017-06-07 15:05:42 +0200

    cedar-16

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,31 @@
 #   })
 
 
+chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+
+chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => "/app/.apt/usr/bin/google-chrome" } } : {}
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(
+     app,
+     browser: :chrome,
+     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+     	'chromeOptions' => {
+    	'binary' => "/app/.apt/usr/bin/google-chrome",
+	      'args' => [
+	      	"--headless"
+	      ]
+	    }
+     )
+  )
+end
+
+Capybara.configure do |config|
+  config.run_server = false
+  config.default_driver = :chrome
+  config.app_host = 'https://www.google.com' # change url
+end
+
 
 Selenium::WebDriver::Chrome.driver_path="/chromedriver"
 
